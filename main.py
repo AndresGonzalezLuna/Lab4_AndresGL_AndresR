d92a842f929b22913c793ae4b0d2435f64dae9f7
Andrés Ramírez Villanueva <if726061@iteso.mx>
2023-04-27 10:29:00 -0600

    otrao cambio de descarga de datos

--- main.py
+++ main.py
@@ -24,32 +24,10 @@ async def get_orderbooks(exchanges, run_time, symbol):
 exchanges = ["kucoin", "bittrex", "bitfinex"]
 run_time = 100000000000000000000000000000000000000000
 
-#symbol1 = 'BTC/USDT'
+symbol1 = 'BTC/USDT'
 
 control = 0
 cc=0
-
-#while (control==0):
-    
-#    ETH_BTC1 = asyncio.run(get_orderbooks(exchanges, run_time, symbol1))
-    
-#    if(cc==0):
-#        inicial = ETH_BTC1
-    
-#    if(cc==1):
-#        historial = pd.concat([inicial, ETH_BTC1], axis=0)
-    
-#    if(cc>=2):
-#        historial = pd.concat([historial, ETH_BTC1], axis=0)
-#        historial.reset_index(drop=True, inplace=True)
-#        
-        
-#    cc+=1
-
-#historial.to_csv('files\orderbooks_27abr2023_BTCUSDT.csv') 
-#historial.to_json('files\orderbooks_27abr2023_BTCUSDT.json')
-
-symbol1 = 'ETH/EUR'
 
 while (control==0):
     
@@ -64,8 +42,30 @@ while (control==0):
     if(cc>=2):
         historial = pd.concat([historial, ETH_BTC1], axis=0)
         historial.reset_index(drop=True, inplace=True)
-                
+        
+        
     cc+=1
+
+#historial.to_csv('files\orderbooks_27abr2023_BTCUSDT.csv') 
+#historial.to_json('files\orderbooks_27abr2023_BTCUSDT.json')
+
+#symbol1 = 'ETH/EUR'
+
+#while (control==0):
+    
+#    ETH_BTC1 = asyncio.run(get_orderbooks(exchanges, run_time, symbol1))
+    
+#    if(cc==0):
+#        inicial = ETH_BTC1
+    
+#    if(cc==1):
+#        historial = pd.concat([inicial, ETH_BTC1], axis=0)
+    
+#    if(cc>=2):
+#        historial = pd.concat([historial, ETH_BTC1], axis=0)
+#        historial.reset_index(drop=True, inplace=True)
+                
+#    cc+=1
 
 #historial.to_json('files\orderbooks_27abr2023_ETHEUR.json')
 #historial.to_csv('files\orderbooks_27abr2023_ETHEUR.csv')    
