94b6b4bf53d212763e58e35dee04b5ff8bf64124
AndresGonzalezLuna <70516656+AndresGonzalezLuna@users.noreply.github.com>
2023-04-27 15:28:05 -0600

    Correccion de Rolls
    Corregi Rolls Effective Spread para qeu ya funcione como el profe queria.

--- functions.py
+++ functions.py
@@ -36,34 +36,67 @@ def vwap(df):
     
     return pd.concat(dfs, ignore_index=True)
 
-def rolls(df):
-    
+def rolls(df, lag_minutes):
     
     dfs = []
     
     for exchange in list(df['exchange'].unique()):
         
         df4 = df[df['exchange'] == exchange].copy()
-
+        
         df4.set_index('datetime', inplace=True)
         
-        # calculate the returns and lag them by one time period
-        df4['returns'] = np.log(df4['close_price']).diff()
+        df4.index = pd.to_datetime(df4.index) # hacemos index datetime object
         
-        #Arreglar el shift para que sea de 1 minuto
-        df4['lagged_returns'] = df4['returns'].shift(1)
+        # get data by second
+        df4 = df4.resample('T').mean()
         
-        # calculate the covariance matrix of the returns and lagged returns
-        cov_matrix = np.cov(df4[['returns', 'lagged_returns']].dropna().T)
+        # get lag minutes intervals in data    
+
+        hour = pd.to_datetime(df4.index.hour, format='%H')
+        minute = pd.to_datetime(df4.index.minute // lag_minutes * lag_minutes, format='%M')
+
+        df4['5min'] = (hour + pd.TimedeltaIndex(minute.hour * 60 + minute.minute, unit='m')).strftime('%H:%M')
+
+        minutes5 = list(df4['5min'].unique())
         
-        # calculate Roll's spread
-        roll_spread = np.sqrt(cov_matrix[0, 0])
+        # Sacamos lag para cad bin que acabos de crear
         
-        # apply Roll's spread to the timeseries
-        df4['roll_spread'] = roll_spread*2
+        pairs = []
+
+        for i in range(1, len(minutes5)):
         
-        df4 = df4.drop(['returns', 'lagged_returns'], axis=1)
+            returns = np.array(np.log(df4.loc[df4['5min'] == minutes5[i], 'close_price']).diff().dropna()) #Pt
+            lagged_returns = np.array(np.log(df4.loc[df4['5min'] == minutes5[i-1], 'close_price']).diff().dropna()) #Pt-1
+            
+            arrays = [returns, lagged_returns]
+            arrays = sorted(arrays, key=lambda x:len(x)) # ordenamos de chico a grande por length
+            
+            arrays[0] = np.pad(arrays[0], (0, len(arrays[1]) - len(arrays[0])), mode='constant') # agregamos ceros para que el legth coincida
+            
+            cov = np.cov(arrays[0], arrays[1])[0,1] # Sacamos covarianza entre Pt y Pt-1
+            
+            roll_spread =2*np.sqrt(abs(cov)) # Sacamos Rolls Effective Spread
+            
+            pair = [minutes5[i], roll_spread] # Creamos par de bin con su respectivo Rolls
+            
+            pairs.append(pair) # Agregmaos a lista
+            
+            
+            
+        df_pairs = pd.DataFrame(pairs, columns = ['5min', 'Rolls Spread']) 
         
-        dfs.append(df4)
+        df4['Time'] = df4.index
         
-    return pd.concat(dfs, ignore_index=True)
+        df_final = pd.merge(df4, df_pairs, on=['5min'], how = 'outer')
+        
+        df_final.set_index('Time', inplace=True)
+        
+        df_final = df_final.resample(str(lag_minutes)+'T').mean()
+        
+        df_final['exchange'] = exchange
+        
+        dfs.append(df_final)
+        
+    #return pd.concat(dfs, ignore_index=True) #Juntamos todos los Df
+    return dfs
